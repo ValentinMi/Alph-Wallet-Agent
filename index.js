97921012a3e9a1d6af0090a4316d5bfa61c9e4bf
Valentin Misiaszek <mowerv6@gmail.com>
2024-08-30 19:27:54 +0200

    use node-cron

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import {
   prettifyAttoAlphAmount,
 } from "@alephium/web3";
 import { PrivateKeyWallet } from "@alephium/web3-wallet";
+import cron from "node-cron"
 
 const BENEFICIARY_ADDRESS = process.env.BENEFICIARY_ADDRESS;
 const privateKey = process.env.INTERMEDIARY_ADDRESS_PRIVATE_KEY;
@@ -18,9 +19,13 @@ const wallet = new PrivateKeyWallet({
 
 const interval = process.env.CALL_INTERVAL_IN_MILLISECOND ||  6 * 60 * 60 * 1000 // Default is 6 hours;
 
-main().then(() => {
-  setInterval(main, interval);
-}).catch(e => log(e.message));
+main()
+cron.schedule(convertMillisToCron(interval), async () => {
+  await main()
+});
+
+// Prevent the Node.js process from exiting
+setInterval(() => {}, 1 << 30);
 
 async function main() {
   const balance = await getBalance(wallet.address);
@@ -85,3 +90,27 @@ function getFormattedDate() {
 function log(message) {
   console.log(`[${getFormattedDate()}] -- ${message}`);
 }
+
+
+/**
+ * @param {number} ms
+ * @returns {string}
+ */
+function convertMillisToCron(ms) {
+  const totalSeconds = Math.floor(ms / 1000);
+  const totalMinutes = Math.floor(totalSeconds / 60);
+  const totalHours = Math.floor(totalMinutes / 60);
+  const totalDays = Math.floor(totalHours / 24);
+
+  if (totalSeconds < 60) {
+    return `*/${totalSeconds} * * * * *`; // Every X seconds
+  } else if (totalMinutes < 60) {
+    return `0 */${totalMinutes} * * * *`; // Every X minutes at second 0
+  } else if (totalHours < 24) {
+    return `0 0 */${totalHours} * * *`;   // Every X hours at minute 0, second 0
+  } else {
+    return `0 0 0 */${totalDays} * *`;    // Every X days at 00:00:00
+  }
+}
+
+
